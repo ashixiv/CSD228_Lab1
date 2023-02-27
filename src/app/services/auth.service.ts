9cae732f8a044343acc8a0f4a36652fc2115a249
ashixiv <90415857+ashixiv@users.noreply.github.com>
2023-02-27 08:48:21 -0500

    Lab 3 part A done

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/auth';
+import { DatabaseService } from './database.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  userData: any = {
+    email: ""
+    }
+   
   loggedIn: Boolean = false;
 
-  constructor() {
+  constructor(private database: DatabaseService) {
   this.loggedIn = false;
   }
 
@@ -22,4 +29,25 @@ export class AuthService {
     throw new Error("Invalid credentials");
     }
   }
+  getUserData() {
+    return this.userData;
+    }
+    signInWithGooglePopUp() {
+      let provider = new firebase.GoogleAuthProvider();
+      firebase.signInWithPopup(firebase.getAuth(), provider).then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      // result.credential.accessToken;
+      this.database.writeLoginToDatabase(result.user.email!);
+      
+      this.loggedIn = true;
+      this.userData.email = result.user.email;
+      }).catch((error) => {
+      // Handle Errors here.
+      //error.code;
+      //error.message;
+      //error.email;
+      console.log(error.message);
+      });
+      }
+     
 }
